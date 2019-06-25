ab4e7b079628475f34284fe9fcbe938cd279ecc2
MJPCBA01CATARG <marcelo.possetti@groupecat.com.ar>
2019-06-25 10:00:41 -0300

    Insert Lugar

---
+++ slnHorario/waEntitys/Lugar.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace waEntitys
+{
+    public class Lugar
+    {
+        public string Descripcion { get; set; }
+    }
+}
---
+++ slnHorario/waRepositorys/LugarRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+using Dapper;
+using waEntitys;
+
+namespace waRepositorys
+{
+    public class LugarRepository
+    {
+        string _db = "";
+
+        public bool InsertLugar(Lugar empleado)
+        {
+            try
+            {
+                using (IDbConnection db = new SqlConnection(_db))
+                {
+                    DynamicParameters param = new DynamicParameters();
+                    db.Open();
+                    param.Add("@Descripcion", empleado.Descripcion);
+                    db.Execute("sp_AltaLugar", param, commandType: CommandType.StoredProcedure);
+                    db.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+            return true;
+        }
+    }
+}
