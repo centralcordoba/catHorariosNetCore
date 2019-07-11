5fa964d34303907146e7749ea09d5fba99abb0e1
LAE <andres.esquitin@groupecat.com.ar>
2019-07-11 11:12:21 -0300

    CentroRepository-Service

--- slnHorario/waRepositorys/CentrosRepository.cs
+++ slnHorario/waRepositorys/CentrosRepository.cs
@@ -1,10 +1,86 @@
-﻿using System;
+﻿using Dapper;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Text;
+using waEntitys;
 
 namespace waRepositorys
 {
     public class CentrosRepository
     {
+        static string _db = "Data Source=ARGSQL03;Initial Catalog=HorariosPrueba;User ID=sa;Password=bfg2007;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+        /// <summary>
+        /// Creacion de código para ejecutar sp UpdateCentros
+        /// </summary>
+        /// <param @Nombre es la descripción de la Categoría></param>
+        /// <param @Codigo es la cantidad de horas de permanencia que debe tener el empleado en la Empresa asociado a la Categoría></param>
+        
+        public static bool UpdateCentros(Centros centro)
+        {
+            try
+            {
+                using (IDbConnection db = new SqlConnection(_db))
+                {
+                    DynamicParameters param = new DynamicParameters();
+                    db.Open();
+                    param.Add("@Nombre", centro.Nombre);
+                   
+                    db.Execute("sp_UpdateCentros", param, commandType: CommandType.StoredProcedure);
+                    db.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                return false;
+                throw;
+            }
+            return true;
+
+        }
+        public static bool AltaCentros(Centros centro)
+        {
+            try
+            {
+                using (IDbConnection db = new SqlConnection(_db))
+                {
+                    DynamicParameters param = new DynamicParameters();
+                    db.Open();
+                    param.Add("@Nombre", centro.Nombre);
+                   
+                    db.Execute("sp_AltaCentros", param, commandType: CommandType.StoredProcedure);
+                    db.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+            return true;
+        }
+        public static List<Centros> GetAllCentros()
+        {
+            ///lstCentros List<Centros> new= List<Centros>();
+            try
+            {
+                using (IDbConnection db = new SqlConnection(_db))
+                {
+                    DynamicParameters param = new DynamicParameters();
+                    db.Open();
+
+                    return db.Query<Centros>("sp_UpdateCentros", commandType: CommandType.StoredProcedure).AsList();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+
+
+        }
     }
 }
--- slnHorario/waServices/CentrosService.cs
+++ slnHorario/waServices/CentrosService.cs
@@ -1,10 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using waEntitys;
 
 namespace waServices
 {
     public class CentrosService
     {
+
+        public class CentroService
+        {
+            public static bool UpdateCentros(Centros centro)
+            {
+                if (waRepositorys.CentrosRepository.UpdateCentros(centro))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+
+
+            }
+            public static bool AltaCentros(Centros centro)
+            {
+                if (waRepositorys.CentrosRepository.AltaCentros(centro))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+
+
+            }
+            public static List<Centros> GetAllCategoriaEmpleado()
+            {
+                return (waRepositorys.CentrosRepository.GetAllCentros());
+
+
+
+            }
+        }
+
     }
 }
