98ac0bcc8a3194b9e3e7050179038f3a393eeaea
Francisco Ramirez <francisco.ramirez@groupecat.com.ar>
2019-07-18 10:04:08 -0300

    turnos

--- slnHorario/waEntitys/Turnos.cs
+++ slnHorario/waEntitys/Turnos.cs
@@ -7,6 +7,14 @@ namespace waEntitys
     public class Turnos
     {
         public string Nombre  { get; set; }
-       
+        public int idturno { get; set; }
+        public string Inicio { get; set; }
+        public string Fin { get; set; }
+        public int Categoria { get; set; }
+        public int Tolerancia { get; set; }
+        public string InicioF { get; set; }
+        public string FinF { get; set; }
+        public string Estado { get; set; }
+        public int Idturno { get; set; }
     }
 }
--- slnHorario/waRepositorys/TurnosRepository.cs
+++ slnHorario/waRepositorys/TurnosRepository.cs
@@ -1,10 +1,100 @@
-﻿using System;
+﻿using Dapper;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Text;
+using waEntitys;
 
 namespace waRepositorys
 {
     public class TurnosRepository
     {
+        static string _db = "Data Source=ARGSQL03;Initial Catalog=HorariosPrueba;User ID=sa;Password=bfg2007;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+        /// <summary>
+        /// Creacion de código para ejecutar sp UpdateEmpresa
+        /// </summary>
+        /// <param @Descripcion es la descripción de la Categoría></param>
+        /// <param @CantHsxTurtno es la cantidad de horas de permanencia que debe tener el empleado en la Empresa asociado a la Categoría></param>
+        /// <param @CantHsFinde es la cantidad de horas de permanencia el fin de semana que debe tener el empleado en la Empresa asociado a la Categoría></param>
+        /// <returns></returns>
+        public static bool UpdateTurnos(Turnos turnos)
+        {
+            try
+            {
+                using (System.Data.IDbConnection db = new System.Data.SqlClient.SqlConnection(_db))
+                {
+                    DynamicParameters param = new DynamicParameters();
+                    db.Open();
+                    param.Add("@Nombre", turnos.Nombre);
+                    param.Add("@Inicio", turnos.Inicio);
+                    param.Add("@Fin", turnos.Fin);
+                    param.Add("@Categoria", turnos.Categoria);
+                    param.Add("@Tolerancia", turnos.Tolerancia);
+                    param.Add("@InicioF", turnos.InicioF);
+                    param.Add("@FinF", turnos.FinF);
+                    param.Add("@Idturno", turnos.idturno);
+                    db.Execute("sp_UpdateTurnos", param, commandType: CommandType.StoredProcedure);
+                    db.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                return false;
+                throw;
+            }
+            return true;
+
+        }
+        public static bool Altaturnos(Turnos turnos)
+        {
+            try
+            {
+                using (System.Data.IDbConnection db = new System.Data.SqlClient.SqlConnection(_db))
+                {
+                    DynamicParameters param = new DynamicParameters();
+                    db.Open();
+                    param.Add("@nombre", turnos.Nombre);
+                    param.Add("@Inicio", turnos.Inicio);
+                    param.Add("@Fin", turnos.Fin);
+                    param.Add("@Categoria", turnos.Categoria);
+                    param.Add("@Tolerancia", turnos.Tolerancia);
+                    param.Add("@InicioF", turnos.InicioF);
+                    param.Add("@Finf", turnos.FinF);
+                    db.Execute("sp_Altaturnos", param, commandType: CommandType.StoredProcedure);
+                    db.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+            return true;
+        }
+        public static List<Turnos> GetAllturnos()
+        {
+            ///lstCategoriaEmpleado List<CategoriaEmpleado> new= List<CategoriaEmpleado>();
+            try
+            {
+                using (System.Data.IDbConnection db = new System.Data.SqlClient.SqlConnection(_db))
+                {
+                    DynamicParameters param = new DynamicParameters();
+                    db.Open();
+
+                    return db.Query<Turnos>("sp_ListarTurnos", commandType: CommandType.StoredProcedure).AsList();
+                    db.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+
+
+        }
+
     }
 }
+
