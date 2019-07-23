dddfb02491e529667ec2aa59589dfd52d95a9bef
LAE <andres.esquitin@groupecat.com.ar>
2019-07-23 10:01:47 -0300

    Service-Repositorio Login

---
+++ slnHorario/waServices/LoginService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using waEntitys;
+
+namespace waServices
+{
+	public class LoginService
+	{
+	
+		public static bool Login(Login login)
+		{
+			if (waRepositorys.LoginRepository.Login(login))
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+
+
+		}
+		public static List<Login> GetAllLogin()
+		{
+			return (waRepositorys.LoginRepository.GetAllLogin());
+
+
+
+		}
+	}
+}
