156e5ef61d4c6682bb166b94abf428a1a953c6b3
MJP <marcelo.possetti@groupecat.com.ar>
2019-07-23 09:33:24 -0300

    Repository y Service Empleado

--- slnHorario/waServices/EmpleadoServices.cs
+++ slnHorario/waServices/EmpleadoServices.cs
@@ -1,10 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using waEntitys;
 
 namespace waServices
 {
     public class EmpleadoServices
     {
+        public static bool UpdateEmpleado(Empleados empleado)
+        {
+            if (waRepositorys.EmpleadoRepository.UpdateEmpleado(empleado))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+
+        }
+        public static bool AltaEmpleado(Empleados empleado)
+        {
+            if (waRepositorys.EmpleadoRepository.AltaEmpleado(empleado))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+
+        }
+        public static List<Empleados> GetAllEmpleado(int Legajo)
+        {
+            return (waRepositorys.EmpleadoRepository.GetAllEmpleado(Legajo));
+
+
+
+        }
     }
 }
+
