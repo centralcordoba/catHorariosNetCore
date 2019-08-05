198c6bcd24d6875598bc31e5c064beedf7a0c711
LAE <andres.esquitin@groupecat.com.ar>
2019-08-05 10:11:20 -0300

    Controller Turnos

---
+++ slnHorario/waRestApi/Controllers/TurnosController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using waEntitys;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace waRestApi.Controllers
+{
+	[Route("api/[controller]")]
+	public class TurnosController : Controller
+	{
+		// GET: api/<controller>
+		[HttpGet]
+		public List<Turnos> Get()
+		{
+			try
+			{
+				List<Turnos> lstTurnos = new List<Turnos>();
+				return lstTurnos = waServices.TurnosService.GetAllTurnos();
+			}
+			catch (Exception ex)
+
+			{
+				throw;
+			}
+		}
+
+		// GET api/<controller>/5
+		[HttpGet("{id}")]
+		public string Get(int id)
+		{
+			return "value";
+		}
+
+		// POST api/<controller>
+		[HttpPost]
+		public void Post([FromBody]string value)
+		{
+		}
+
+		// PUT api/<controller>/5
+		[HttpPut("{id}")]
+		public void Put(int id, [FromBody]string value)
+		{
+		}
+
+		// DELETE api/<controller>/5
+		[HttpDelete("{id}")]
+		public void Delete(int id)
+		{
+		}
+	}
+}
