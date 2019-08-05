3413efab3aa3c7a8a1dc1ff1bd0d13c155515f50
LAE <andres.esquitin@groupecat.com.ar>
2019-08-05 09:44:14 -0300

    Service Turnos llamada Repository (Update, GetAll, Insert)

--- slnHorario/waServices/TurnosService.cs
+++ slnHorario/waServices/TurnosService.cs
@@ -1,10 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using waEntitys;
 
 namespace waServices
 {
     public class TurnosService
     {
-    }
+
+		public static bool UpdateTurnos(Turnos turnos)
+		{
+			if (waRepositorys.TurnosRepository.UpdateTurnos(turnos))
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+
+
+		}
+		public static bool AltaTurnos(Turnos turnos)
+		{
+			if (waRepositorys.TurnosRepository.Altaturnos(turnos))
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+
+
+		}
+		public static List<Turnos> GetAllTurnos()
+		{
+			return (waRepositorys.TurnosRepository.GetAllturnos());
+
+
+
+		}
+	}
 }
+
