c4dc9f4d6c69904327cd63807f2fa90d095dde83
LAE <andres.esquitin@groupecat.com.ar>
2019-08-06 09:15:28 -0300

    EmpresaService Update-getAll-Insert

--- slnHorario/waServices/EmpresasService.cs
+++ slnHorario/waServices/EmpresasService.cs
@@ -1,10 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using waEntitys;
 
 namespace waServices
 {
-    public class EmpresasService
-    {
-    }
+	public class EmpresasService
+	{
+		public static bool UpdateEmpresas(Empresas empresa)
+		{
+			if (waRepositorys.EmpresaRepository.UpdateEmpresa(empresa))
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+
+
+		}
+		public static bool AltaEmpresas(Empresas empresa)
+		{
+			if (waRepositorys.EmpresaRepository.AltaEmpresa(empresa))
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+
+
+		}
+		public static List<Empresas> GetAllempresa()
+		{
+			return (waRepositorys.EmpresaRepository.GetAllempresa());
+
+
+
+		}
+	}
 }
+
+
+		
+
+
+		
+
+
+
+
+
+
