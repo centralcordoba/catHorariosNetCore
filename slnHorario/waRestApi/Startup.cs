aaaf1a82122f7eeb6388749de101d29b1672da27
Emanuel <lejiei@gmail.com>
2019-08-13 08:03:27 -0300

    Add Cors Policy

--- slnHorario/waRestApi/Startup.cs
+++ slnHorario/waRestApi/Startup.cs
@@ -26,6 +26,12 @@ namespace waRestApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
+            {
+                builder.AllowAnyOrigin()
+                       .AllowAnyMethod()
+                       .AllowAnyHeader();
+            }));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,7 +45,7 @@ namespace waRestApi
             {
                 app.UseHsts();
             }
-
+            app.UseCors("MyPolicy");
             app.UseHttpsRedirection();
             app.UseMvc();
         }
